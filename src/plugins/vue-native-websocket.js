1b4b0c3b7e2c646984114890985e5a56e0eaf9fc
Max Walther <maxiwalther@mac.com>
2020-09-24 19:51:30 +0200

    Switch to production api stage

--- src/plugins/vue-native-websocket.js
+++ src/plugins/vue-native-websocket.js
@@ -3,12 +3,12 @@ import VueNativeSock from 'vue-native-websocket'
 
 import store from '../store'
 
-Vue.use(VueNativeSock, 'wss://r8sc9tucc2.execute-api.eu-central-1.amazonaws.com/dev', {
+Vue.use(VueNativeSock, 'wss://r8sc9tucc2.execute-api.eu-central-1.amazonaws.com/prod', {
     reconnection: true,
     reconnectionDelay: 3000,
     store: store,
     format: 'json',
-    passToStoreHandler: function(eventName, event) {
+    passToStoreHandler: function (eventName, event) {
         if (!eventName.startsWith('SOCKET_')) { return }
         let target = eventName.toUpperCase()
         let msg = event
