59ffec811e34c8d310d7ee25aed87aff1d1eb99b
Max Walther <maxiwalther@mac.com>
2020-12-12 17:25:40 +0100

    Add collaborative book app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <v-app>
+    <v-app-bar app dense flat>
+      <v-icon>mdi-book-open-variant</v-icon>
+      <v-spacer />
+      <v-toolbar-title align-center>Collaborative Book</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
     <router-view />
   </v-app>
 </template>
